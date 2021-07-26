0257470e9b620867fe942b18a87a246f8b741d01
wangshixiang <402314889@qq.com>
2021-07-26 14:29:57 +0800

    feat: 重构项目结构

---
+++ internal/rbac/go.mod
@@ -0,0 +1,3 @@
+module github.com/keepondream/RBAC_service/internal/rbac
+
+go 1.16
--- migrations/20210401154101_init_id_generator.down.sql
+++
@@ -1,3 +0,0 @@
--- 删除雪花算法方法时需要清除所有依赖的表才能够正确删除
-drop SEQUENCE IF EXISTS global_id_seq;
-drop FUNCTION IF EXISTS id_generator();
